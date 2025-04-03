c40dc56c044b3cf54a7814075ffd4e77a9f70b86
gabriel <gabrielpissininmenossi@gmail.com>
2025-04-03 18:04:24 -0300

    criando novamente outra branch, agora mais organizada

--- cmake-build-debug/CMakeFiles/inode.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/inode.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/matheus/Documentos/Unoeste/Sistemas Operacionais II/inode/main.c" "CMakeFiles/inode.dir/main.c.o" "gcc" "CMakeFiles/inode.dir/main.c.o.d"
+  "/home/gabriel/CLionProjects/inode/main.c" "CMakeFiles/inode.dir/main.c.o" "gcc" "CMakeFiles/inode.dir/main.c.o.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
